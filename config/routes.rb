ba8583137710a359939fd42692f27c821c18f727
Steve Goldband <steve@goldband.com>
2012-06-23 14:31:40 -0700

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
+  match '/signup',  to: 'users#new'
   root to: 'static_pages#home'
 
   # The priority is based upon order of creation:
