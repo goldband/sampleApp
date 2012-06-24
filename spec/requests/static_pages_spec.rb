649edf342944b1b672bf9e217dd4309781ed387b
Steve Goldband <steve@goldband.com>
2012-06-23 22:34:59 -0700

    nasic user model

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,6 +10,11 @@ describe "Static pages" do
     it { should have_selector('h1',    text: 'Sample App') }
     it { should have_selector('title', text: full_title('')) }
     it { should_not have_selector 'title', text: '| Home' }
+
+    # click_link doesnt work!
+   # click_link "About"
+  # page.should have_selector 'title', text: full_title('About Us')
+
   end
 
   describe "Help page" do
@@ -23,7 +28,7 @@ describe "Static pages" do
     before { visit about_path }
 
     it { should have_selector('h1',    text: 'About') }
-    it { should have_selector('title', text: full_title('About Us')) }
+    it { should have_selector('title', text: full_title('About')) }
   end
 
   describe "Contact page" do
@@ -32,4 +37,22 @@ describe "Static pages" do
     it { should have_selector('h1',    text: 'Contact') }
     it { should have_selector('title', text: full_title('Contact')) }
   end
+
+  describe "LayoutLinks" do
+
+    it "should have the right links on the layout" do
+      visit root_path
+      click_link "About"
+      response.should have_selector('title', :content => "About")
+      #click_link "Home"
+      #response.should have_selector('title', :content => "Home")
+      #click_link "Help"
+      #response.should have_selector('title', :content => "Help")
+      #click_link "Contact"
+      #response.should have_selector('title', :content => "Contact")
+      #click_link "Sign up now!"
+      #response.should have_selector('title', :content => "Sign up")
+    end
+  end
 end
+
