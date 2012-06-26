a34521f2113cacaade4f803b26beef5d5e2496ab
Steve Goldband <steve@goldband.com>
2012-06-26 10:45:30 -0700

    finished to 7.4.4

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Michael Hartlxx"
+    email    "michael@examplexx.com"
+    password "foobarx"
+    password_confirmation "foobarx"
+  end
+end
